37a8f5d1e9eba460fb2400f5e4a0a98ec97a4bcc
Max <tutv95@gmail.com>
2019-06-20 20:07:47 +0700

    updated

--- stream-server.js
+++ stream-server.js
@@ -1,5 +1,60 @@
-const express = require('express');
+const express = require('express')
+const app = express()
 
-const app = express();
+const fs = require('fs')
+const path = require('path')
 
-// todo: write code here
+const _isNumber = (text) => {
+    const reg = /^\d+$/
+
+    return reg.test(text)
+}
+
+const _format = (content) => {
+    const blocks = content.split('.')
+
+    return blocks
+        .map((block) => {
+            return block ? block.trim() : ''
+        })
+        .map(block => {
+            if (_isNumber(block)) return false
+
+            return block
+        })
+        .filter(Boolean)
+        .map(block => {
+            const regex = /\,\s\d+/gi
+
+            return block.replace(regex, '')
+        })
+        .map(block => {
+            const regex = /\,\s([A-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪỬỮỰỲỴÝỶỸ])/g
+
+            return block.replace(regex, '.$1')
+        })
+        .map(block => {
+            return block.replace(/\./g, '\n')
+        })
+        .join('\n')
+}
+
+app.get('/tho', (req, res) => {
+    const file = path.join(__dirname, 'truyen-kieu.txt')
+    const stream = fs.createReadStream(file, {encoding: 'utf8'})
+
+    res.set({'content-type': 'text/plain; charset=utf-8'});
+    stream.on('data', (content) => {
+        const formatted = _format(content)
+
+        res.write(formatted)
+    })
+
+    stream.on('close', () => {
+        res.end()
+    })
+})
+
+app.listen(2000, () => {
+    console.log('App is listening on port 2000...')
+})
